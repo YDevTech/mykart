f2e52c5936f41bbd93b9173a9e239bd62a2e473a
Yadudev <vyadudev@gmail.com>
2024-01-18 04:06:14 +0530

    home page, product page finished

--- mykart/products/views.py
+++ mykart/products/views.py
@@ -3,8 +3,15 @@ from .models import Product
 from django.core.paginator import Paginator
 
 # Create your views here.
+
 def index(request):
-    return render(request,'index.html')
+    featured_products = Product.objects.order_by('priority')[:4]
+    latest_products = Product.objects.order_by('-id')[:4]
+    context={
+        'featured_products':featured_products,
+        'latest_products':latest_products,
+    }
+    return render(request, 'index.html',context)
 
 def list_products(request):
     """_summary_
@@ -16,14 +23,16 @@ def list_products(request):
     Returns:
         _type_: _description_
     """
-    page=1
+    page = 1
     if request.GET:
-        request.GET.get('page',1)
-    product_list=Product.objects.all()
-    product_paginator=Paginator(product_list,4)
-    product_list=product_paginator.get_page(page)
-    context={'products':product_list}
-    return render(request,'products.html',context)
+        page = request.GET.get('page', 1)
+    product_list = Product.objects.order_by('-priority')
+    product_paginator = Paginator(product_list, 2)
+    product_list = product_paginator.get_page(page)
+    context = {'products': product_list}
+    return render(request, 'products.html', context)
 
-def detail_products(request):
-    return render(request,'product_detail.html')
+def detail_products(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
